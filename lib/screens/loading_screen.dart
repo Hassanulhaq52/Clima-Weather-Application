a1c5125e8e7df75353acbd4fb7b9d3275bf8462c
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-10-05 16:09:49 +0500

    Showing a Spinner While the User Waits.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
-
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 const apikey = '26045213ee14d654983128cc5c270adb';
 
@@ -29,16 +31,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(latitude);
     print(longitude);
 
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey');
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey');
 
     var weatherData = await networkHelper.getData();
 
+    Navigator.push(this.context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
-
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
